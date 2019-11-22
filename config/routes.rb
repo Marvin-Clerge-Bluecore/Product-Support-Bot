fe4b32bf12753ee26897d50deee1dd98f98f905a
Marvin Clerge <marvin.clerge@bluecore.com>
2019-11-22 14:34:48 -0500

    check for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'user#index', as: 'user'
+  get '/', to: 'user#index', as 'user'
+  # get '/', to: 'user#index', as: 'user'
   post '/update', to: 'user#update', as: 'update'
   get '/escalate', to: 'user#escalate', as: 'escalate'
 end
